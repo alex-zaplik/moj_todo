981d76c8bebb9e88be59f8d47b5607a7dce78530
Mikołaj Pietrek <mikolaj.pietrek@gmail.com>
2019-09-12 07:39:33 +0200

    Admin panel; simple tasks view; example Bootstrap template

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Task
+
+def index(request):
+    """
+    A simple view that display all tasks in database
+    """
+    
+    task_list = Task.objects.order_by('-deadline')[:5]
+    context = {'task_list': task_list}
+    return render(request, 'todo_app/index.html', context)
