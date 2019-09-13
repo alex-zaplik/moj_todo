fc4d73fce05a16d9800a02585532d6ef12f10c2c
Alek <zaplikpl@gmail.com>
2019-09-13 14:25:49 +0200

    Added a simple tables list. Did some refactoring on the Tasks view

--- todo_app/models.py
+++ todo_app/models.py
@@ -47,7 +47,7 @@ class Task(NamedModel):
 
     column = models.ForeignKey(Column, on_delete=models.CASCADE)
     description = models.TextField()
-    deadline = models.DateField(blank=True)
+    deadline = models.DateField(null=True, blank=True)
     done = models.BooleanField(default=False)
 
     def is_near_to_deadline(self):
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 
-from .models import Task
+from .models import Table, Column, Task
 
 def index(request):
     """
-    A simple view that display all tasks in database
+    A simple view that display all tables in the database
     """
     
-    task_list = Task.objects.order_by('-deadline')[:5]
+    # TODO: Only get tables that belong to the user
+    table_list = Table.objects.all()
+    context = {'table_list': table_list}
+    return render(request, 'todo_app/tables.html', context)
+
+
+def table(request, pk):
+    """
+    A simple view that display all tasks in a table
+    """
+
+    # TODO: Only show tables that belong to the user
+    # TODO: Ignoring columns now, but that has to change
+    task_list = Task.objects.filter(column__table__pk=pk).order_by('-deadline')[:5]
     context = {'task_list': task_list}
-    return render(request, 'todo_app/index.html', context)
+    return render(request, 'todo_app/table.html', context)
