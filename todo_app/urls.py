7c845c03fba687a03bb5854f2a6869093faec467
Alex <zaplikpl@gmail.com>
2019-09-22 13:24:28 +0200

    Updated permission handling

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add_task/<int:pk>/', views.add_task, name='task_save'),
     path('edit_task/<int:pk>/', views.edit_task, name='task_edit'),
     path('move_task/', views.move_task, name='move_task'),
+    path('denied/', views.access_denied, name='denied')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.views.generic import CreateView
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
+from django.core.exceptions import PermissionDenied
 
 from .models import Table, Column, Task
 from .forms import ColumnForm, TaskFrom
@@ -27,17 +28,21 @@ def table(request, pk):
 
     table_remind_list = [x for x in Table.objects.all() if request.user in x.users.all()]
     task_remind_list = Task.objects.all()
-    curr_table = Table.objects.get(pk=pk)
 
-    if request.user in curr_table.users.all():
-        column_form = ColumnForm()
-        task_form = TaskFrom()
-        column_list = Column.objects.filter(table__pk=pk)
-        task_list = Task.objects.filter(column__table__pk=pk).order_by('-deadline')
-        context = {'user': request.user, 'page_title': curr_table.name, 'table_remind_list': table_remind_list, 'task_remind_list' : task_remind_list, 'column_list': column_list, 'column_form': column_form, 'task_form': task_form, 'tab_id': pk}
-    else:
-        context = {'user': request.user, 'page_title': "Access denied", 'table_remind_list': table_remind_list, 'task_remind_list' : task_remind_list, 'column_list': [], 'column_form': None, 'task_form': None, 'tab_id': ""}
-    return render(request, 'todo_app/table.html', context)
+    try:
+        curr_table = Table.objects.get(pk=pk)
+        if request.user in curr_table.users.all():
+            column_form = ColumnForm()
+            task_form = TaskFrom()
+            column_list = Column.objects.filter(table__pk=pk)
+            task_list = Task.objects.filter(column__table__pk=pk).order_by('-deadline')
+            context = {'user': request.user, 'page_title': curr_table.name, 'table_remind_list': table_remind_list, 'task_remind_list' : task_remind_list, 'column_list': column_list, 'column_form': column_form, 'task_form': task_form, 'tab_id': pk}
+        else:
+            raise PermissionDenied()
+    
+        return render(request, 'todo_app/table.html', context)
+    except:
+        return redirect(reverse('denied'))
 
 
 @require_POST
@@ -47,13 +52,22 @@ def add_column(request, pk):
     Source: ColumnForm
     """
 
-    if request.method == 'POST':
-        column = Column(table=Table.objects.get(pk=pk))
-        form = ColumnForm(request.POST, instance=column)
-        if form.is_valid():
-            form.save()
+    try:
+        table = Table.objects.get(pk=pk)
 
-    return redirect(reverse('table', kwargs={'pk': pk}))
+        if request.user not in table.users.all():
+            raise PermissionDenied()
+
+        if request.method == 'POST':
+            column = Column(table=table)
+            form = ColumnForm(request.POST, instance=column)
+            if form.is_valid():
+                form.save()
+
+        return redirect(reverse('table', kwargs={'pk': pk}))
+
+    except (PermissionDenied, Table.DoesNotExist):
+        return redirect(reverse('denied'))
 
 
 @require_POST
@@ -63,13 +77,28 @@ def add_task(request, pk):
     Source: TaskForm
     """
 
-    if request.method == 'POST':
-        # TODO: Check if the key is present
-        form = TaskFrom(request.POST, instance=Task(column_id=int(request.POST.get('column'))))
-        if form.is_valid():
-            form.save()
+    try:
+        table = Table.objects.get(pk=pk)
 
-    return redirect(reverse('table', kwargs={'pk': pk}))
+        if request.user not in table.users.all():
+            raise PermissionDenied()
+
+        if request.method == 'POST':
+            # TODO: Check if the key is present
+            column = Column.objects.get(pk=int(request.POST.get('column')))
+            if column.table.pk != pk:
+                print(column.table.pk, pk)
+                raise django.core.exceptions.PermissionDenied()
+
+            form = TaskFrom(request.POST, instance=Task(column_id=column.id))
+
+            if form.is_valid():
+                form.save()
+
+        return redirect(reverse('table', kwargs={'pk': pk}))
+
+    except (PermissionDenied, Table.DoesNotExist):
+        return redirect(reverse('denied'))
 
 
 @require_POST
@@ -79,13 +108,22 @@ def edit_task(request, pk):
     Source: TaskForm
     """
 
-    if request.method == 'POST' and False: # TODO: For now this request is ignored
-        # TODO: Check if the keys are present
-        form = TaskFrom(request.POST, instance=Task(id=int(request.POST.get('task')), column_id=int(request.POST.get('column'))))
-        if form.is_valid():
-            form.save()
+    try:
+        table = Table.objects.get(pk=pk)
 
-    return redirect(reverse('table', kwargs={'pk': pk}))
+        if request.user not in table.users.all():
+            raise PermissionDenied()
+
+        if request.method == 'POST' and False: # TODO: For now this request is ignored
+            # TODO: Check if the keys are present
+            form = TaskFrom(request.POST, instance=Task(id=int(request.POST.get('task')), column_id=int(request.POST.get('column'))))
+            if form.is_valid():
+                form.save()
+
+        return redirect(reverse('table', kwargs={'pk': pk}))
+
+    except (PermissionDenied, Table.DoesNotExist):
+        return redirect(reverse('denied'))
 
 
 @require_POST
@@ -97,23 +135,38 @@ def move_task(request):
 
     data = {}
 
-    if request.method == 'POST':
-        # TODO: Check if the keys are present and if no 404 arises
-        task = Task.objects.get(pk=int(request.POST.get('task')))
-        target = Column.objects.get(pk=int(request.POST.get('target')))
+    try:
+        if request.method == 'POST':
+            # TODO: Check if the keys are present and if no 404 arises
+            task = Task.objects.get(pk=int(request.POST.get('task')))
+            target = Column.objects.get(pk=int(request.POST.get('target')))
 
-        # Checking if the origin and target columns are in the same table
-        if task.column.table == target.table:
-            # Move task
-            task.column = target
-            task.save()
+            # Checking if the origin and target columns are in the same table
+            if task.column.table == target.table:
+                if request.user not in target.table.users:
+                    raise PermissionDenied()
 
-            data['success'] = True
-            data['url'] = reverse('table', kwargs={ 'pk': target.table.pk })
-        else:
-            # Send error
-            data['success'] = True
-            data['msg'] = "The origin and target columns are not the same"
-        
-    return JsonResponse(data)
+                # Move task
+                task.column = target
+                task.save()
+
+                data['success'] = True
+                data['url'] = reverse('table', kwargs={ 'pk': target.table.pk })
+            else:
+                # Send error
+                data['success'] = True
+                data['msg'] = "The origin and target columns are not the same"
             
+        return JsonResponse(data)    
+
+    except (PermissionDenied, Table.DoesNotExist):
+        return redirect(reverse('denied'))
+
+
+def access_denied(request):
+    table_list = [x for x in Table.objects.all() if request.user in x.users.all()]
+    task_list = Task.objects.all()
+
+    context = {'page_title': "Access denied", 'page_subtitle': request.GET.get('subtitle', ''), 'table_remind_list': table_list, 'task_remind_list' : task_list}
+
+    return render(request, 'todo_app/access_denied.html', context)
