4e24f6fa29e3ecf27237071a3d8464565f9b6513
Alex <zaplikpl@gmail.com>
2019-09-24 17:38:17 +0200

    Switched  to class based views

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:pk>/', views.table, name='table'),
-    path('add_column/<int:pk>/', views.add_column, name='column_save'),
-    path('add_task/<int:pk>/', views.add_task, name='task_save'),
-    path('edit_task/<int:pk>/', views.edit_task, name='task_edit'),
-    path('move_task/', views.move_task, name='move_task'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.TableView.as_view(), name='table'),
+    path('add_column/<int:pk>/', views.AddColumnView.as_view(), name='column_save'),
+    path('add_task/<int:pk>/', views.AddTaskView.as_view(), name='task_save'),
+    path('edit_task/<int:pk>/', views.EditTaskView.as_view(), name='task_edit'),
+    path('move_task/<int:pk>/', views.MoveTaskView.as_view(), name='move_task'),
     path('denied/', views.access_denied, name='denied')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import CreateView
+from django.views import View
+from django.views.generic.base import TemplateResponseMixin
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from django.core.exceptions import PermissionDenied
@@ -11,159 +13,157 @@ from .forms import ColumnForm, TaskFrom
 import urllib.parse
 
 
-def index(request):
+class CheckedTableView(View):
+
+    not_exist_msg = "Table does not exist"
+    not_allowed_msg = "You are not allowed to access this table"
+
+    def check_table(self, request, *args, **kwargs):
+        assert 'pk' in kwargs
+
+        try:
+            curr_table = Table.objects.get(pk=kwargs['pk'])
+            if request.user not in curr_table.users.all():
+                raise PermissionDenied(self.not_allowed_msg)
+        except Table.DoesNotExist:
+            raise PermissionDenied(self.not_exist_msg)
+    
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.method.lower() in self.http_method_names:
+            self.check_table(request, *args, **kwargs)
+            handler = getattr(self, "checked_" + request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+
+class IndexView(TemplateResponseMixin, View):
     """
-    A simple view that display tables that belong to the user
+    A simple view that displays tables that belong to the user
     """
 
-    table_list = [x for x in Table.objects.all() if request.user in x.users.all()]
-    task_list = Task.objects.all()
+    template_name = 'todo_app/tables.html'
 
-    context = {'table_list': table_list, 'table_remind_list': table_list, 'task_remind_list' : task_list}
-    return render(request, 'todo_app/tables.html', context)
+    def get(self, request):
+        table_list = [x for x in Table.objects.all() if request.user in x.users.all()]
+        task_list = Task.objects.all()
+
+        context = {'table_list': table_list, 'table_remind_list': table_list, 'task_remind_list' : task_list}
+        return self.render_to_response(context)
 
 
-def table(request, pk):
+class TableView(TemplateResponseMixin, CheckedTableView):
     """
     A simple view that display all tasks in a table that belong to the user
     """
 
-    table_remind_list = [x for x in Table.objects.all() if request.user in x.users.all()]
-    task_remind_list = Task.objects.all()
+    template_name = 'todo_app/table.html'
+    not_allowed_msg = "You are not allowed to view this table"
 
-    try:
+    def checked_get(self, request, pk):
+        table_remind_list = [x for x in Table.objects.all() if request.user in x.users.all()]
+        task_remind_list = Task.objects.all()
+
         curr_table = Table.objects.get(pk=pk)
-        if request.user in curr_table.users.all():
-            column_form = ColumnForm()
-            task_form = TaskFrom()
-            column_list = Column.objects.filter(table__pk=pk)
-            task_list = Task.objects.filter(column__table__pk=pk).order_by('-deadline')
-            context = {'user': request.user, 'page_title': curr_table.name, 'table_remind_list': table_remind_list, 'task_remind_list' : task_remind_list, 'column_list': column_list, 'column_form': column_form, 'task_form': task_form, 'tab_id': pk}
-        else:
-            raise PermissionDenied("You are not allowed to view this table")
-    
-        return render(request, 'todo_app/table.html', context)
-    
-    except Table.DoesNotExist:
-        raise PermissionDenied("Table does not exist")
+        column_form = ColumnForm()
+        task_form = TaskFrom()
+        column_list = Column.objects.filter(table__pk=pk)
+        task_list = Task.objects.filter(column__table__pk=pk).order_by('-deadline')
+        context = {'user': request.user, 'page_title': curr_table.name, 'table_remind_list': table_remind_list, 'task_remind_list' : task_remind_list, 'column_list': column_list, 'column_form': column_form, 'task_form': task_form, 'tab_id': pk}
+        
+        return self.render_to_response(context)
 
 
-@require_POST
-def add_column(request, pk):
+class AddColumnView(CheckedTableView):
     """
     A temporary view that handles POST method
     Source: ColumnForm
     """
 
-    try:
+    not_allowed_msg = "You are not allowed to add to this table"
+
+    def checked_post(self, request, pk):
         table = Table.objects.get(pk=pk)
+        column = Column(table=table)
+        form = ColumnForm(request.POST, instance=column)
+        if form.is_valid():
+            form.save()
+        
+        return redirect(reverse('table', kwargs={'pk': pk}))
 
-        if request.user not in table.users.all():
-            raise PermissionDenied("You are not allowed to add to this table")
 
-        if request.method == 'POST':
-            column = Column(table=table)
-            form = ColumnForm(request.POST, instance=column)
-            if form.is_valid():
-                form.save()
+class AddTaskView(CheckedTableView):
+    """
+    A temporary view that handles POST method
+    Source: ColumnForm
+    """
+
+    not_allowed_msg = "You are not allowed to add to this table"
+
+    def checked_post(self, request, pk):
+        table = Table.objects.get(pk=pk)
+        # TODO: Check if the key is present
+        column = Column.objects.get(pk=int(request.POST.get('column')))
+        if column.table.pk != pk:
+            raise PermissionDenied("Table id dont's match (org:%d,target:%d)" % (column.table.pk, pk))
+        
+        form = TaskFrom(request.POST, instance=Task(column_id=column.id))
+
+        if form.is_valid():
+            form.save()
 
         return redirect(reverse('table', kwargs={'pk': pk}))
 
-    except Table.DoesNotExist:
-        raise PermissionDenied("Table does not exist")
 
-
-@require_POST
-def add_task(request, pk):
+class MoveTaskView(CheckedTableView):
     """
-    A temporary view that handles POST method parameters
-    Source: TaskForm
+    A temporary view that handles POST method
+    Source: ColumnForm
     """
 
-    try:
+    not_allowed_msg = "You are not allowed to move tasks in this table"
+
+    def checked_post(self, request, pk):
+        data = {}
+
+        # TODO: Check if the keys are present and if no 404 arises
+        task = Task.objects.get(pk=int(request.POST.get('task')))
+        target = Column.objects.get(pk=int(request.POST.get('target')))
+
+        # Checking if the origin and target columns are in the same table
+        if task.column.table == target.table and target.table.pk == pk:
+            # Move task
+            task.column = target
+            task.save()
+
+            data['success'] = True
+            data['url'] = reverse('table', kwargs={ 'pk': target.table.pk })
+        else:
+            # Send error
+            data['success'] = True
+            data['msg'] = "The origin and target columns are not the same"
+        
+        return JsonResponse(data)
+
+
+class EditTaskView(CheckedTableView):
+    """
+    A temporary view that handles POST method
+    Source: ColumnForm
+    """
+
+    not_allowed_msg = "You are not allowed to edit tasks in this table"
+
+    def checked_post(self, request, pk):
         table = Table.objects.get(pk=pk)
-
-        if request.user not in table.users.all():
-            raise PermissionDenied("You are not allowed to add to this table")
-
-        if request.method == 'POST':
-            # TODO: Check if the key is present
-            column = Column.objects.get(pk=int(request.POST.get('column')))
-            if column.table.pk != pk:
-                print(column.table.pk, pk)
-                raise django.core.exceptions.PermissionDenied()
-
-            form = TaskFrom(request.POST, instance=Task(column_id=column.id))
-
-            if form.is_valid():
-                form.save()
+        # TODO: Check if the keys are present
+        # TODO: Ignored for now, awaiting implementation
+        # form = TaskFrom(request.POST, instance=Task(id=int(request.POST.get('task')), column_id=int(request.POST.get('column'))))
+        # if form.is_valid():
+        #     form.save()
 
         return redirect(reverse('table', kwargs={'pk': pk}))
-
-    except Table.DoesNotExist:
-        raise PermissionDenied("Table does not exist")
-
-
-@require_POST
-def edit_task(request, pk):
-    """
-    A temporary view that handles POST method parameters
-    Source: TaskForm
-    """
-
-    try:
-        table = Table.objects.get(pk=pk)
-
-        if request.user not in table.users.all():
-            raise PermissionDenied("You are not allowed to edit this table")
-
-        if request.method == 'POST' and False: # TODO: For now this request is ignored
-            # TODO: Check if the keys are present
-            form = TaskFrom(request.POST, instance=Task(id=int(request.POST.get('task')), column_id=int(request.POST.get('column'))))
-            if form.is_valid():
-                form.save()
-
-        return redirect(reverse('table', kwargs={'pk': pk}))
-
-    except Table.DoesNotExist:
-        raise PermissionDenied("Table does not exist")
-
-
-@require_POST
-def move_task(request):
-    """
-    A view that handles moving a tast to a different column
-    of the same table
-    """
-
-    data = {}
-
-    try:
-        if request.method == 'POST':
-            # TODO: Check if the keys are present and if no 404 arises
-            task = Task.objects.get(pk=int(request.POST.get('task')))
-            target = Column.objects.get(pk=int(request.POST.get('target')))
-
-            # Checking if the origin and target columns are in the same table
-            if task.column.table == target.table:
-                if request.user not in target.table.users:
-                    raise PermissionDenied("You are not allowed to edit this table")
-
-                # Move task
-                task.column = target
-                task.save()
-
-                data['success'] = True
-                data['url'] = reverse('table', kwargs={ 'pk': target.table.pk })
-            else:
-                # Send error
-                data['success'] = True
-                data['msg'] = "The origin and target columns are not the same"
-            
-        return JsonResponse(data)    
-
-    except Table.DoesNotExist:
-        raise PermissionDenied("Table does not exist")
 
 
 def access_denied(request, exception):
